f0d1da6e15423e058fd9622425aa0ae468779b61
Amrish Kumar <amrish@hopscotch.in>
2016-02-15 02:25:11 +0530

    First version

---
+++ sh-website/src/main/java/ch/swisshimmel/website/config/WebConfig.java
@@ -0,0 +1,17 @@
+package ch.swisshimmel.website.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class WebConfig implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext)
+			throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
