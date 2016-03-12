a46a79aaab222bc11c9aa696efad2dc5a2f8119e
manohar <manohar@192.168.0.10>
2016-03-12 20:47:35 +0100

    sdfs

---
+++ sh-website/src/main/java/ch/swisshimmel/website/common/constants/OccasionConstants.java
@@ -0,0 +1,12 @@
+package ch.swisshimmel.website.common.constants;
+
+public interface OccasionConstants {
+
+    
+    public static String OCCASION_VALUE_DS = "occaValueDs";
+
+    public static String ADD_EVENT_TIME = "addEventTime";
+    
+    public static String ADD_OCCASION = "addOccasion";
+
+}
